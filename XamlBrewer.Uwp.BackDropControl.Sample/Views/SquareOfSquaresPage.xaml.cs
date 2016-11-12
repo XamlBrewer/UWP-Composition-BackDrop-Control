959492c35e821f5ac3487312891059c3e4a08333
Diederik Krols <diederik_krols@hotmail.com>
2016-11-12 16:23:48 +0100

    SquareOfSquares.

--- XamlBrewer.Uwp.BackDropControl.Sample/Views/SquareOfSquaresPage.xaml.cs
+++ XamlBrewer.Uwp.BackDropControl.Sample/Views/SquareOfSquaresPage.xaml.cs
@@ -1,6 +1,6 @@
-﻿using Windows.UI;
+﻿using System;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
+using XamlBrewer.Uwp.Controls;
 
 namespace XamlBrewer.Uwp.BackDropControl.Sample
 {
@@ -9,6 +9,22 @@ namespace XamlBrewer.Uwp.BackDropControl.Sample
         public SquareOfSquaresPage()
         {
             this.InitializeComponent();
+            Loaded += SquareOfSquaresPage_Loaded;
+        }
+
+        private void SquareOfSquaresPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var random = new Random((int)DateTime.Now.Ticks);
+            foreach (var square in SquareOfSquares.Squares)
+            {
+                square.Content = new BackDrop
+                {
+                    Height = square.ActualHeight,
+                    Width = square.ActualWidth,
+                    TintColor = square.RandomColor(),
+                    BlurAmount = random.Next(50)
+                };
+            }
         }
     }
 }
